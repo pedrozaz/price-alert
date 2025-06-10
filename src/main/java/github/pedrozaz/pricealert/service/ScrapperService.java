1dd5a4f9da95f603bc47147e38a7af7b4cfa61e0
Guilherme <guilhermebarb0sa@proton.me>
2025-06-10 19:33:17 -0300

    Web scrapper created // aint tested yet

---
+++ src/main/java/github/pedrozaz/pricealert/service/ScrapperService.java
@@ -0,0 +1,27 @@
+package github.pedrozaz.pricealert.service;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import github.pedrozaz.pricealert.exception.ScrapperException;
+
+public class ScrapperService {
+
+    public String findPrice(String url, String selector) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
+            Page page = browser.newPage();
+
+            page.navigate(url);
+            page.waitForSelector(selector);
+
+            String price = page.locator(selector).textContent();
+
+            browser.close();
+            return price;
+        } catch (Exception e) {
+            throw new ScrapperException("Error scrapping");
+        }
+    }
+}
